aa68193f0175128811747b9d36ba402aabf8cbe4
Johannes Borchard <johannesborchard@gmail.com>
2025-06-14 18:02:53 +0200

    h1 responsive fix

--- src/components/typography/H1.tsx
+++ src/components/typography/H1.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react"
 
 export function H1({ children }: PropsWithChildren<{}>) {
   return (
-    <h1 className="mb-3 scroll-m-20 text-center text-4xl leading-16 font-bold tracking-tight text-balance sm:text-5xl lg:text-6xl">
+    <h1 className="mb-3 scroll-m-20 text-center text-4xl leading-tight font-bold tracking-tight text-balance sm:text-5xl sm:leading-16 lg:text-6xl">
       {children}
     </h1>
   )
