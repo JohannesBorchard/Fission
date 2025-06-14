724d7d8cd9b53d91845cb3cc6d658b9c8064884d
Johannes Borchard <johannesborchard@gmail.com>
2025-06-14 19:00:24 +0200

    wording

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -4,7 +4,7 @@ import { H1 } from "@/components/common/elements/H1"
 import { P } from "@/components/common/elements/P"
 import Section from "@/components/layout/Section"
 import { Button } from "@/components/ui/Button"
-import { Atom, Box, CircleAlert, Database, DollarSign, Shield, type LucideIcon } from "lucide-react"
+import { Atom, Box, Database, DollarSign, Shield, Signpost, type LucideIcon } from "lucide-react"
 
 export default function IndexPage() {
   return (
@@ -18,15 +18,14 @@ export default function IndexPage() {
 
 function HeroSection() {
   return (
-    /* className="container mx-auto my-20 px-5" */
     <Section className="mt-10 mb-20 flex flex-col items-center space-y-8">
       <Button variant="secondary" className="mb-3 rounded-full">
         Follow along on Twitter
       </Button>
-      <H1>An example app built using Next.js 13 server components.</H1>
+      <H1>An example app built using React 19 and shadcn.</H1>
       <P className="">
-        I'm building a web app with Next.js 13 and open sourcing everything. Follow along as we
-        figure this out together.
+        The main goal is learning modern software architecture best practices. A focus lies on
+        minimizing cognitive load from tailwindCSS without using @apply.
       </P>
       <div className="flex gap-3">
         <Button size="lg">Get Started</Button>
@@ -46,34 +45,34 @@ interface Feature {
 
 const features: Feature[] = [
   {
-    Icon: CircleAlert,
-    title: "Next.js 13",
-    description: "App dir, Routing, Layouts, Loading UI und API-Routen."
+    Icon: Atom,
+    title: "React 19",
+    description: "Client-Components, Hooks."
   },
   {
-    Icon: Atom,
-    title: "React 18",
-    description: "Server- und Client-Components, Hooks, Streaming."
+    Icon: Signpost,
+    title: "React Router",
+    description: "Declarative component-based routing."
+  },
+  {
+    Icon: Box,
+    title: "shadcn",
+    description: "UI-Components based on Radix UI and tailwindCSS."
   },
   {
     Icon: Database,
     title: "Database",
-    description: "ORM mit Prisma, deployed auf PlanetScale."
-  },
-  {
-    Icon: Box,
-    title: "Components",
-    description: "UI-Components mit Radix UI, gestylt via Tailwind CSS."
+    description: "TODO."
   },
   {
     Icon: Shield,
     title: "Authentication",
-    description: "Auth via NextAuth.js und Edge Middleware."
+    description: "TODO."
   },
   {
     Icon: DollarSign,
-    title: "Subscriptions",
-    description: "Free & paid Subscriptions via Stripe."
+    title: "Stripe",
+    description: "TODO."
   }
 ]
 
@@ -82,18 +81,15 @@ function FeaturesSection() {
     <Section className="my-20 space-y-8">
       <H1>Features</H1>
       <P>
-        This project is an experiment to see how a modern app, with features like auth,
-        subscriptions, API routes, and static pages would work in Next.js 13 app dir.
+        This project is an experiment to see how modern and scalable web development works with an
+        increasing set of state of the art technologies.
       </P>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
         {features.map((f) => (
           <FeatureCard key={f.title} Icon={f.Icon} title={f.title} description={f.description} />
         ))}
       </div>
-      <P>
-        Taxonomy also includes a blog and a full-featured documentation site built using
-        Contentlayer and MDX.
-      </P>
+      <P>Other features will be included here.</P>
     </Section>
   )
 }
@@ -101,10 +97,10 @@ function FeaturesSection() {
 function LicenseSection() {
   return (
     <Section className="my-20">
-      <H1>Proudly Open Source</H1>
+      <H1>Open Source License</H1>
       <P>
-        Taxonomy is open source and powered by open source software. The code is available on{" "}
-        <A href="https://www.github.com" target="_blank">
+        Fission is open source and powered by open source software. The code will be available on{" "}
+        <A href="https://github.com/JohannesBorchard" target="_blank">
           GitHub
         </A>
       </P>
