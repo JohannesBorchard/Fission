8dd77090317283c31273001549bbbdcf6eeb0a05
Johannes Borchard <johannesborchard@gmail.com>
2025-06-14 19:27:35 +0200

    footer ui polish

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,16 +4,16 @@ import { ModeToggle } from "../ui/ModeToggle"
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center space-y-4 px-8 py-5 sm:flex-row sm:justify-between sm:space-y-0">
-      <div className="text-center sm:text-left">
-        <Zap className="text-primary/90 mb-1 inline size-6" />
-        <span className="text-primary ml-2">
+    <footer className="flex flex-col items-center space-y-4 px-8 py-5 md:flex-row md:justify-between md:space-y-0">
+      <div className="text-center md:text-left">
+        <Zap className="text-muted-foreground mb-1 inline size-6" />
+        <span className="text-muted-foreground ml-2">
           Built by{" "}
-          <A href="https://github.com/JohannesBorchard" target="_blank">
+          <A href="https://github.com/JohannesBorchard" target="_blank" className="text-primary/80">
             Johannes Borchard
           </A>
           . Hosted by Vercel. Initial layout inspiration from{" "}
-          <A href="https://tx.shadcn.com/" target="_blank">
+          <A href="https://tx.shadcn.com/" target="_blank" className="text-primary/80">
             Taxonomy
           </A>
           .
