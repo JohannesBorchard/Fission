b0e308effa5f2300852b95ef68d639bd00bb9226
Johannes Borchard <johannesborchard@gmail.com>
2025-06-15 19:27:52 +0200

    add shadcn badge

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -8,7 +8,7 @@ import { Link } from "react-router"
 export default function NotFound() {
   return (
     <Section className="my-20 flex flex-col items-center space-y-8">
-      <H1>404 Not Found</H1>
+      <H1>404 Not (Yet) Found</H1>
       <P>
         Our quantum state hasn’t decohered into this page yet. Respawn in a different universe or
         come back once the superposition has collapsed.
