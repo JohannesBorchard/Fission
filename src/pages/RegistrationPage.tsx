e645654637f24ed36896042b8e4e0085461e2a57
Johannes Borchard <johannesborchard@gmail.com>
2025-06-16 11:40:30 +0200

    functional registration

--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -3,9 +3,45 @@ import { Button } from "@/components/ui/Button"
 import { Card, CardContent } from "@/components/ui/Card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { supabase } from "@/lib/supabaseClient"
+import { useState } from "react"
 import { Link } from "react-router"
+import { toast } from "sonner"
 
 export function RegistrationPage() {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+
+  async function handleSignup(formData: FormData) {
+    setLoading(true)
+    setError(null)
+
+    try {
+      const email = formData.get("email")
+      const password = formData.get("password")
+
+      if (typeof email !== "string" || typeof password !== "string") {
+        throw new Error("Please enter valid email and password.")
+      }
+      const { error } = await supabase.auth.signUp({ email, password })
+
+      if (error) {
+        throw new Error(error.message)
+      }
+      toast.success("Registration successful!", {
+        description: "Please open the confirmation email to log-in."
+      })
+      // TODO: Weiterleitung: navigate("/secret-page"), setUnverified(true)
+    } catch (err) {
+      // Supabase or validation errors
+      const message = err instanceof Error ? err.message : "An unexpected error has occurred"
+      setError(message)
+      toast.error(message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <Section>
       <h2 className="mb-6 text-center text-4xl font-bold tracking-tight sm:text-5xl">
@@ -17,11 +53,17 @@ export function RegistrationPage() {
       <div className="mx-auto flex max-w-lg flex-col gap-6">
         <Card>
           <CardContent>
-            <form>
+            <form action={handleSignup}>
               <div className="flex flex-col gap-6">
                 <div className="grid gap-3">
                   <Label htmlFor="email">Email</Label>
-                  <Input id="email" type="email" placeholder="your@email.com" required />
+                  <Input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="your@email.com"
+                    required
+                  />
                 </div>
                 <div className="grid gap-3">
                   <Label htmlFor="password" className="h-[20px]">
@@ -29,6 +71,7 @@ export function RegistrationPage() {
                   </Label>
                   <Input
                     id="password"
+                    name="password"
                     type="password"
                     placeholder="Create a strong password"
                     required
