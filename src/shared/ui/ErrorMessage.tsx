302427eddfb2d486b5ef06ad4bc7f5f5566dc588
Johannes Borchard <johannesborchard@gmail.com>
2025-06-30 18:22:19 +0200

    reload

--- src/shared/ui/ErrorMessage.tsx
+++ src/shared/ui/ErrorMessage.tsx
@@ -5,7 +5,7 @@ export function ErrorMessage({ error }: { error: string }) {
     <div className="text-center text-red-500">
       <div className="mb-3">Error: {error}</div>
       <Button onClick={() => window.location.reload()} className="mt-4">
-        Retry
+        Reload Page
       </Button>
     </div>
   )
