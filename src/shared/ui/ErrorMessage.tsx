efc9fef8e61e96278c3767e99897469326f1b7f1
Johannes Borchard <johannesborchard@gmail.com>
2025-06-30 18:15:48 +0200

    error styling

--- src/shared/ui/ErrorMessage.tsx
+++ src/shared/ui/ErrorMessage.tsx
@@ -3,7 +3,7 @@ import { Button } from "./Button"
 export function ErrorMessage({ error }: { error: string }) {
   return (
     <div className="text-center text-red-500">
-      Error: {error}
+      <div className="mb-3">Error: {error}</div>
       <Button onClick={() => window.location.reload()} className="mt-4">
         Retry
       </Button>
